eedc30fb76fd445f4e9ac0d8bdf3b352c69a173c
Mirian Okradze <okradzemirian@gmail.com>
2022-03-08 17:46:36 +0400

    feat: styled header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
 const Header = () => (
-  <div>
-    <h1>CrowdCoin</h1>
+  <div className='flex justify-between border-2 border-gray-200'>
+    <h1 className='text-lg py-2 border-r-2 border-gray-200 px-4'>CrowdCoin</h1>
 
-    <button>Create Campaign</button>
-    <button>+</button>
+    <div className='flex'>
+      <button className='border-x-2 border-gray-200 px-4'>Campaigns</button>
+      <button className='px-4'>+</button>
+    </div>
   </div>
 )
 
