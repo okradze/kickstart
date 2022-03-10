fea81d956d871ac596bf73556217ff83f9abaa52
Mirian Okradze <okradzemirian@gmail.com>
2022-03-10 15:31:05 +0400

    feat: new campaign factory contract

--- src/contracts.ts
+++ src/contracts.ts
@@ -3,5 +3,5 @@ import CampaignFactory from '../ethereum/build/CampaignFactory.json'
 
 export const campaignFactoryContract = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x6A3B1a2377779996099A8bBCf1Ac8b4d0451d088',
+  '0x338b473c2a35972BAdDc9eC3BBC565a845038AEE',
 )
