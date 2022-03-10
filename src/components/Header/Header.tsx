4e9e72c3023244fbd71a2cf119db9fafd16ee238
Mirian Okradze <okradzemirian@gmail.com>
2022-03-10 15:08:53 +0400

    feat: header navigation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,20 @@
+import Link from 'next/link'
+
 const Header = () => (
   <div className='flex justify-between border-2 border-gray-200'>
-    <h1 className='text-lg py-2 border-r-2 border-gray-200 px-4'>CrowdCoin</h1>
+    <Link href='/'>
+      <a>
+        <h1 className='text-lg py-2 border-r-2 border-gray-200 px-4'>CrowdCoin</h1>
+      </a>
+    </Link>
 
     <div className='flex'>
-      <button className='border-x-2 border-gray-200 px-4'>Campaigns</button>
-      <button className='px-4'>+</button>
+      <Link href='/'>
+        <a className='border-x-2 border-gray-200 px-4 flex items-center'>Campaigns</a>
+      </Link>
+      <Link href='/campaigns/new'>
+        <a className='px-4 flex items-center'>+</a>
+      </Link>
     </div>
   </div>
 )
