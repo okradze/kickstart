43908c59a1b6923d0ceae31add80dd0ad168a1bc
Mirian Okradze <okradzemirian@gmail.com>
2022-03-10 15:27:24 +0400

    feat: campaign contract features

---
+++ src/pages/campaigns/[address].tsx
@@ -0,0 +1,9 @@
+const CampaignDetails = () => {
+  return (
+    <div>
+      <h1>Campaign Details</h1>
+    </div>
+  )
+}
+
+export default CampaignDetails
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,13 +9,18 @@ type HomePageProps = {
 const HomePage: NextPage<HomePageProps> = ({ campaigns }) => {
   return (
     <div>
-      <h1>Campaigns</h1>
+      <h1>Open Campaigns</h1>
       <div>
-        {campaigns.map((campaign) => (
-          <p key={campaign}>{campaign}</p>
+        {campaigns.map((address) => (
+          <div key={address}>
+            <p>{address}</p>
+            <Link href={`/campaigns/${address}`}>
+              <a>View Campaign</a>
+            </Link>
+          </div>
         ))}
       </div>
-      <Link href='/'>
+      <Link href='/campaigns/new'>
         <a>Create Campaign</a>
       </Link>
     </div>
