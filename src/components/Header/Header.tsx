ea8ce8702a5bcc885ee971b79fbdef4214dec14d
Mirian Okradze <okradzemirian@gmail.com>
2022-03-12 12:13:49 +0400

    feat: styled header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 
 const Header = () => (
-  <div className='flex justify-between border-2 border-gray-200'>
+  <div className='flex justify-between rounded-md border-2 border-gray-200 mt-2'>
     <Link href='/'>
       <a>
         <h1 className='text-lg py-2 border-r-2 border-gray-200 px-4'>CrowdCoin</h1>
