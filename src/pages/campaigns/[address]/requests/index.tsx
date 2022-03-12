d6b6ffc6367df08084035c9686f80a0cfecc74f6
Mirian Okradze <okradzemirian@gmail.com>
2022-03-12 12:08:56 +0400

    feat: fetch campaign requests data

--- src/pages/campaigns/[address]/requests/index.tsx
+++ src/pages/campaigns/[address]/requests/index.tsx
@@ -1,8 +1,22 @@
-import { NextPage } from 'next'
+import { GetServerSideProps, NextPage } from 'next'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { getCampaignContract } from '../../../../contracts'
 
-const RequestsPage: NextPage = () => {
+interface CampaignRequest {
+  description: string
+  value: string
+  recipient: string
+  complete: boolean
+  approvalCount: string
+}
+
+type RequestsPageProps = {
+  requests: CampaignRequest[]
+  requestsCount: string
+}
+
+const RequestsPage: NextPage<RequestsPageProps> = ({ requestsCount, requests }) => {
   const router = useRouter()
 
   return (
@@ -13,6 +27,36 @@ const RequestsPage: NextPage = () => {
       </Link>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const campaignContract = getCampaignContract(query.address as string)
+
+  const requestsCount = await campaignContract.methods.getRequestsCount().call()
+
+  const requests = await Promise.all(
+    Array(parseInt(requestsCount))
+      .fill(null)
+      .map(async (_, index) => {
+        const { description, value, recipient, complete, approvalCount } =
+          await campaignContract.methods.requests(index).call()
+
+        return {
+          description,
+          value,
+          recipient,
+          complete,
+          approvalCount,
+        }
+      }),
+  )
+
+  return {
+    props: {
+      requestsCount,
+      requests,
+    },
+  }
 }
 
 export default RequestsPage
