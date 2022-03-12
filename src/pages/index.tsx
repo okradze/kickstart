4ae5bf4f601500557db12956a5681b16c2147e4b
Mirian Okradze <okradzemirian@gmail.com>
2022-03-12 12:35:13 +0400

    feat: styled campaigns list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,19 +10,23 @@ const HomePage: NextPage<HomePageProps> = ({ campaigns }) => {
   return (
     <div>
       <h2 className='text-xl font-bold mb-4'>Open Campaigns</h2>
-      <div>
-        {campaigns.map((address) => (
-          <div key={address} className='border-2 border-gray-200 rounded-md p-3'>
-            <p className='text-lg font-bold mb-1'>{address}</p>
-            <Link href={`/campaigns/${address}`}>
-              <a className='text-cyan-700'>View Campaign</a>
-            </Link>
-          </div>
-        ))}
+      <div className='flex gap-8 items-start'>
+        <div className='grow'>
+          {campaigns.map((address) => (
+            <div key={address} className='border-2 border-gray-200 rounded-md p-3'>
+              <p className='text-lg font-bold mb-1'>{address}</p>
+              <Link href={`/campaigns/${address}`}>
+                <a className='text-cyan-700'>View Campaign</a>
+              </Link>
+            </div>
+          ))}
+        </div>
+        <Link href='/campaigns/new'>
+          <a className='font-bold bg-sky-600 text-white py-2 px-6 rounded inline-block'>
+            Create Campaign
+          </a>
+        </Link>
       </div>
-      <Link href='/campaigns/new'>
-        <a>Create Campaign</a>
-      </Link>
     </div>
   )
 }
